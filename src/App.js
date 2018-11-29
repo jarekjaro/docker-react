169c109bb8a8e4ac36e6ce00d6253760163f182d
jarekjaro <krzeminskijarek@gmail.com>
2018-11-29 12:50:11 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React BRO!
+            I was changed on the feature branch!
           </a>
         </header>
       </div>
